a2c4ab88d80d8b1d716d37d10480cf4d3a8a1f76
mizchi <miz404@gmail.com>
2017-04-24 17:03:08 +0900

    createCacheKey works

--- src/createCacheKey.js
+++ src/createCacheKey.js
@@ -1,16 +1,13 @@
 /* @flow */
-import { CompiledCacheStrategy } from './types'
 import toParams from './toParams'
+import type { CompiledCacheStrategy } from './types'
 
 const createCacheKey: CompiledCacheStrategy[] => string => Promise<string> =
   strategies => async url => {
     const st = strategies.find(s => s.compiledPattern.test(url))
     if (st) {
-      const m = st.compiledPattern.exec(url)
-      if (m) {
-        const params = toParams(st.compiledPattern, m)
-        return st.createCacheKey(params)
-      }
+      const params = toParams(st.compiledPattern, url)
+      return st.createCacheKey(params)
     }
     throw new Error('createCacheKey: should not come here after isUrlCacheable')
   }
--- test/createCacheKey.test.js
+++ test/createCacheKey.test.js
@@ -1,6 +1,27 @@
 /* @flow */
 import test from 'ava'
+import compileCacheStrategies from '../src/compileCacheStrategies'
+import createCacheKey from '../src/createCacheKey'
 
-test('pass', t => {
+const myStrategies = [
+  {
+    pattern: '/items/:id',
+    createCacheKey (params: { id: string }) {
+      return 'cache:' + params.id
+    }
+  }
+]
+
+test('returns key if url is included', async t => {
+  const compiled = compileCacheStrategies(myStrategies)
+  const key = await createCacheKey(compiled)('/items/aaa')
+  t.is(key, 'cache:aaa')
+})
+
+test('throws error if url is not included', async t => {
+  const compiled = compileCacheStrategies(myStrategies)
+  await t.throws(
+    createCacheKey(compiled)('/xxx/yyy')
+  )
   t.pass()
 })
