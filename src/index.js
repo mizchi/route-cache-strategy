a0ad4551839a8bde8367e6bec11c1ee2fab660e8
mizchi <miz404@gmail.com>
2017-05-15 17:13:12 +0900

    Export isUrlCacheable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* @flow */
 
 export { default } from './createCacher'
-export { default as isUrlCacheable } from './isUrlCacheable'
+export { isUrlCacheableOnClient as isUrlCacheable } from './isUrlCacheable'
 export { default as compileCacheStrategies } from './compileCacheStrategies'
--- src/isUrlCacheable.js
+++ src/isUrlCacheable.js
@@ -1,5 +1,6 @@
 /* @flow */
-import type { CompiledCacheStrategy } from './types'
+import compileCacheStrategies from './compileCacheStrategies'
+import type { CompiledCacheStrategy, CacheStrategy } from './types'
 
 // Return route is to be handled by this cache
 const isUrlCacheable: (
@@ -8,3 +9,9 @@ const isUrlCacheable: (
   strategies.some(st => st.compiledPattern.test(url))
 
 export default isUrlCacheable
+
+// Export this for client
+export const isUrlCacheableOnClient: (
+  CacheStrategy[]
+) => string => boolean = strategies =>
+  isUrlCacheable(compileCacheStrategies(strategies))
